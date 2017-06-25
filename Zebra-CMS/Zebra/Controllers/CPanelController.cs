98614987a2c627fef15be1ce35d751cb4d0850c0
Rishabh Kumar <devileatspie@gmail.com>
2017-06-25 17:35:03 +0530

    Added ZebraAuthorize attribute

---
+++ Zebra-CMS/Zebra/Controllers/CPanelController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zebra.CustomAttributes;
+
+namespace Zebra.Controllers
+{
+    public class CPanelController : Controller
+    {
+        // GET: CPanel
+        public ActionResult Index()
+        {
+            return RedirectToAction("Editor");
+        }
+
+        // GET: CPanel/Editor
+        [ZebraAuthorize(Roles ="Editor")]
+        public ActionResult Editor()
+        {
+            return View();
+        }
+
+        // GET: CPanel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CPanel/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CPanel/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CPanel/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CPanel/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CPanel/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
